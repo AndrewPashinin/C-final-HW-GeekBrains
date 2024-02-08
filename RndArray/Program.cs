16ea22789bb23c50b7bb617d91040f07fc354084
AndrewPashinin <cenizaceniza89@gmail.com>
2024-02-08 12:03:32 +0300

    Перевернутый массив готов

--- RndArray/Program.cs
+++ RndArray/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+
+int[] array = {4, 5, 7, 8, 10, 21, 45};
+
+ReversePrintArray (array, array.Length - 1);
+void ReversePrintArray (int[] array, int i)
+{
+    if (i < 0) return;
+    Console.Write(array[i] + " ");
+    ReversePrintArray(array, i - 1);
+}
